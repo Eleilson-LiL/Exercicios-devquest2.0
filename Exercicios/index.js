be5f211f9d6e141ec6f7890d7fc9e6a34668bc2f
Eleilson silva <eleilsonsilvapaula@hotmail.com>
2026-06-12 19:23:24 -0300

    Feat: Exercicio Js

--- Exercicios/index.js
+++ Exercicios/index.js
@@ -1,22 +1,33 @@
 //Crie variáveis para testar comparações entre tipos iguais e diferentes. Use console.log() para mostrar os resultados das comparações com == e ===. Exemplo: compare um número com outro número, uma string com outra string e um número com uma string contendo o mesmo valor.
 
 // Comparação entre números
-let numero1 = 10;
-let numero2 = 10;
+//let numero1 = 10;
+//let numero2 = 10;
 
-console.log(numero1 == numero2);  // true
-console.log(numero1 === numero2); // true
+//console.log(numero1 == numero2);  // true
+//console.log(numero1 === numero2); // true
 
 // Comparação entre strings
-let texto1 = "Olá";
-let texto2 = "Olá";
+//let texto1 = "Olá";
+//let texto2 = "Olá";
 
-console.log(texto1 == texto2);  // true
-console.log(texto1 === texto2); // true
+//console.log(texto1 == texto2);  // true
+//console.log(texto1 === texto2); // true
 
 // Comparação entre número e string
-let numero = 10;
-let textoNumero = "10";
+//let numero = 10;
+//let textoNumero = "10";
 
-console.log(numero == textoNumero);  // true
-console.log(numero === textoNumero); // false
+//console.log(numero == textoNumero);  // true
+//console.log(numero === textoNumero); // false
+
+//📝 Instrucoes do Exercicio
+//Crie duas variáveis numéricas e faça comparações usando os operadores <, <=, > e >=. Use console.log() para exibir os resultados no console
+
+let numero1 = 10;
+let numero2 = 20;
+
+console.log(numero1 < numero2);  // true
+console.log(numero1 <= numero2); // true
+console.log(numero1 > numero2); // false
+console.log(numero1 >= numero2); // false

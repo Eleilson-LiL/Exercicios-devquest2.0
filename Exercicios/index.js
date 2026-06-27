96addd75ad02007bb549c82c22c37fc676a077d4
Eleilson silva <eleilsonsilvapaula@hotmail.com>
2026-06-27 16:26:22 -0300

    Adicionar novos exercicios de css e javascript

--- Exercicios/index.js
+++ Exercicios/index.js
@@ -24,10 +24,161 @@
 //📝 Instrucoes do Exercicio
 //Crie duas variáveis numéricas e faça comparações usando os operadores <, <=, > e >=. Use console.log() para exibir os resultados no console
 
-let numero1 = 10;
-let numero2 = 20;
+// let numero1 = 10;
+// let numero2 = 20;
 
-console.log(numero1 < numero2);  // true
-console.log(numero1 <= numero2); // true
-console.log(numero1 > numero2); // false
-console.log(numero1 >= numero2); // false
+// console.log(numero1 < numero2);  // true
+// console.log(numero1 <= numero2); // true
+// console.log(numero1 > numero2); // false
+// console.log(numero1 >= numero2); // false
+
+
+//Crie variáveis numéricas para cada operação. Em seguida, faça operações de soma, subtração, multiplicação, divisão e resto da divisão usando essas variáveis. Exiba os resultados no console com console.log()
+
+// const soma1 = 10
+// const soma2 = 5 
+
+// const soma = soma1 + soma2
+
+// console.log(soma)
+
+// const subtracao1 = 15
+// const subtracao2 = 5
+
+// const subtracao = subtracao1 - subtracao2
+// console.log(subtracao);
+
+// const multiplicacao1 = 5
+// const multiplicacao2 = 10
+
+// const multiplicacao = multiplicacao1 * multiplicacao2
+// console.log(multiplicacao)
+
+// let divisao1 = 6
+// let divisao2 = 3
+
+// let divisao = divisao1 / divisao2
+// console.log(divisao)
+
+// const resto1 = 10
+// const resto2 = 2
+
+// const resto = resto1 % resto2 
+// console.log(resto)
+
+//Crie uma variável chamada nota e atribua um valor numérico entre 0 e 10. Use if, else if e else para verificar a nota e exibir no console: 'Aprovado' se a nota for maior ou igual a 7, 'Recuperação' se a nota for entre 5 e 6, e 'Reprovado' se a nota for menor que 5.
+
+// const nota = 10
+
+// if (nota >=7){
+//     console.log("aprovado")
+// } else if (nota >=5 && nota < 7) {
+//     console.log("recuperação")
+// } else {
+//     console.log("reprovado")
+// }
+
+// Crie uma variável chamada diaDaSemana que armazene um número de 1 a 7. Use a estrutura switch para exibir no console o nome do dia correspondente (1 = Domingo, 2 = Segunda, ..., 7 = Sábado). Se o valor não estiver entre 1 e 7, exiba 'Dia inválido'.
+
+// const diaDaSemana = 7
+
+// switch (diaDaSemana) {
+//     case 1:
+//         console.log("Domingo");
+//         break;
+//     case 2:
+//         console.log("segunda");
+//         break;
+//     case 3:
+//         console.log("terça-feira")
+//         break;
+//     case 4:
+//         console.log("Quarta-feira")
+//         break;
+//     case 5:
+//         console.log("Quinta-feira")
+//         break;
+//     case 6:
+//         console.log("Sexta-feira")
+//         break;
+//     case 7:
+//         console.log("Sábado")
+//         break;
+//     default:
+//         console.log("Dia inválido")
+
+// }
+
+// let numero1 = 10
+// let numero2 = 5
+
+// console.log(numero1 + numero2);
+// console.log(numero1 - numero2);
+// console.log(numero1 * numero2);
+// console.log(numero1 / numero2)
+
+// const idade = 20
+// if (idade >=18) {
+//     console.log("Maior de idade")
+// }else ("Menor de idade")
+
+// const nota = 4
+
+// if (nota >= 7) {
+//     console.log("Aprovado")
+// } else if (nota >= 5 && nota < 7) {
+//     console.log("recuperação")
+// } else {
+//     console.log("Reprovado")
+// }
+
+// 📝 Instrucoes do Exercicio
+// Use um loop for para exibir no console os números de 1 a 10. Cada número deve ser mostrado em uma linha diferente usando console.log(). **AVISO:** os resultados serão exibidos na aba de console do navegador
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+// }
+
+// 📝 Instrucoes do Exercicio
+// Use um loop while para exibir no console os números de 1 a 10. Cada número deve ser mostrado em uma linha diferente usando console.log().
+
+// let i = 1;
+//  while (i <=10) {
+//     console.log(i)
+//     i++;
+//  }
+
+// 📝 Instrucoes do Exercicio
+// Use um loop do while para exibir no console os números de 1 a 10. Cada número deve ser mostrado em uma linha diferente usando console.log().
+
+// let i = 1.
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <=10)
+
+// Crie uma função nomeada chamada **saudarUsuario** que receba como parâmetro o nome do usuário. Dentro da função, use console.log() com template literal para exibir a mensagem 'Seja bem vindo' seguida do nome. Em seguida, chame a função passando um nome como argumento para testar.
+
+// function saudarUsuario(nome) {
+//     console.log(`Seja bem vindo ${nome}`);
+// }
+// saudarUsuario("Eleilson")
+
+// Crie uma função chamada **somar** que receba dois números como parâmetros e retorne o resultado da soma. Depois, crie uma variável para armazenar o valor retornado pela função e
+
+// function somar (a, b) {
+//     return a + b;
+// }
+
+// const resultado = somar(10, 5); 
+// console.log(resultado)
+
+// Crie uma arrow function chamada **multiplicar** que receba dois números como parâmetros e retorne o resultado da multiplicação. Em seguida, armazene o valor retornado em uma variável e exiba esse valor no console com console.log().
+
+// const multiplicar = (a, b) => {
+//     return a * b;
+// };
+
+// console.log(multiplicar(5, 4))
+    
+
